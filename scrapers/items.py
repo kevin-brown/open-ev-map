3ccb024b6c4c1635be8dd362475801ec72f0deaf
Kevin Brown-Silva <github@kevin-brown.com>
2024-10-20 18:28:04 -0400

    Add initial Scrapy config

---
+++ scrapers/items.py
@@ -0,0 +1,44 @@
+import scrapy
+
+
+class AddressFeature(scrapy.Item):
+    street_address = scrapy.Field()
+    city = scrapy.Field()
+    state = scrapy.Field()
+    zip_code = scrapy.Field()
+
+
+class LocationFeature(scrapy.Item):
+    latitude = scrapy.Field()
+    longitude = scrapy.Field()
+
+
+class ChargingPortFeature(scrapy.Item):
+    plug = scrapy.Field()
+
+
+class EvseFeature(scrapy.Item):
+    plugs = scrapy.Field()
+
+    network_id = scrapy.Field()
+
+
+class ChargingPointFeature(scrapy.Item):
+    name = scrapy.Field()
+    location = scrapy.Field()
+
+    network_id = scrapy.Field()
+
+    evses = scrapy.Field()
+
+
+class StationFeature(scrapy.Item):
+    name = scrapy.Field()
+    network = scrapy.Field()
+
+    location = scrapy.Field()
+    address = scrapy.Field()
+
+    network_id = scrapy.Field()
+
+    charging_points = scrapy.Field()

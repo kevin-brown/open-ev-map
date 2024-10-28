684b7923a48ffee24b9895a5ae11468d954a2a19
Kevin Brown-Silva <github@kevin-brown.com>
2024-10-27 22:25:33 -0400

    Add initial Blink scraper

--- scrapers/spiders/blink.py
+++ scrapers/spiders/blink.py
@@ -1,11 +1,11 @@
+# Wallingford, CT       Executive Honda                 MQ 200                  L1-0202-2245-001703
+# Greenwich, CT         West Putnam Owner               MQ 200                  L1-0203-2225-000554
 # Ludlow, VT            Main & Mountain                 HQ 200                  L1-0205-W-2233-003986
 # Bedford, MA           Depot Park                      IQ 200                  L1-0207-2005-002228, L1-0207-2028-003958
 # Westwood, MA          Audi Westwood                   IQ 200                  L1-0207-LTE-W-2217-008565
 # Norwell, MA           Audi Norwell                    IQ 200                  L1-0207-ISO-2240-012518, L1-0207-ISO-2240-012465
 # Fall River, MA        Burger King                     IQ 200                  L1-0207-2019-003581
 # Enfield, CT           AAA Enfield                     IQ 200                  L1-0207-LTE-W-2148-006747
-# Wallingford, CT       Executive Honda                 MQ 200                  L1-0202-2245-001703
-# Greenwich, CT         West Putnam Owner               MQ 200                  L1-0203-2225-000554
 # Mansfield, MA         5 Hampshire Street              Series 6 (SemaConnect)  BAE909384
 # Granby, CT            Grande Luxury Apartments        Series 6 (SemaConnect)  BAE907691
 # Hyannis, MA           Hyannis Main Street             Series 6 (SemaConnect)  BAE906774
@@ -35,3 +35,199 @@
 # Wilmington, VT        Town of Wilmington              RT50                    12100261, 12100262
 # Ludlow, VT            Main & Mountain                 RT50                    12100265, 12100267
 # Budd Lake, NJ         Holiday Inn - Budd Lake         ABB Terra 54?           NAMT53-2415-033
+
+from collections import defaultdict
+from scrapers.items import AddressFeature, ChargingPointFeature, ChargingPortFeature, EvseFeature, HardwareFeature, LocationFeature, PowerFeature, StationFeature
+
+import scrapy
+
+
+class BlinkSpider(scrapy.Spider):
+    name = "blink"
+
+    PLUG_FOR_CONNECTOR_TYPE = {
+        "J1772": "J1772",
+        "CHAdeMO": "CHADEMO",
+        "CCS1": "J1772_COMBO",
+    }
+
+    def _query_for_location(self, lat, lon, radius):
+        return {
+            "latitude": lat,
+            "longitude": lon,
+            "latitudeDelta": 0,
+            "longitudeDelta": 0,
+            "deviceCurrentLat": lat,
+            "deviceCurrentLng": lon,
+            "radius": radius,
+        }
+
+    def start_requests(self):
+        query = self._query_for_location(
+            lat=42.13,
+            lon=-71.76,
+            radius=145,
+        )
+
+        yield scrapy.http.JsonRequest(
+            url="https://apigw.blinknetwork.com/nmap/v3/locations/map/pins",
+            data=query,
+            method="POST",
+        )
+
+    def parse(self, response):
+        locations = response.json()
+
+        for location in locations:
+            yield scrapy.http.JsonRequest(
+                url=f"https://apigw.blinknetwork.com/v3/locations/map/{location["locationId"]}",
+                method="GET",
+                meta={
+                    "location": location,
+                },
+                callback=self.parse_station,
+            )
+
+    def parse_station(self, response):
+        station = response.json()
+
+        if station["address"]["countryCode"] != "US":
+            return
+
+        if station["address"]["state"] != "Massachusetts":
+            return
+
+        yield scrapy.http.JsonRequest(
+            url=f"https://apigw.blinknetwork.com/v3/locations/{station["locationId"]}?isPrivateStationReq=false",
+            method="GET",
+            meta={
+                "station": station,
+                "location": response.meta["location"],
+            },
+            callback=self.parse_chargers,
+        )
+
+    def parse_chargers(self, response):
+        response_data = response.json()
+        station = response.meta["station"]
+        location = response.meta["location"]
+
+        address = AddressFeature(
+            street_address=station["address"]["addressLine1"],
+            city=station["address"]["city"],
+            state=station["address"]["state"],
+            zip_code=station["address"]["postalCode"],
+        )
+
+        coordinates = LocationFeature(
+            latitude=location["latitude"],
+            longitude=location["longitude"],
+        )
+
+        charging_points = []
+
+        charging_point_serials = {}
+        ports_for_charging_points = defaultdict(list)
+
+        for charger_class in response_data:
+            for charger in charger_class["chargers"]:
+                if charger["serialNumber"] not in charging_point_serials:
+                    charging_point_serials[charger["serialNumber"]] = ChargingPointFeature(
+                        location=coordinates,
+                        network_id=charger["serialNumber"],
+                        hardware=self.hardware_for_serial_number(charger["serialNumber"], charger),
+                        evses=[],
+                    )
+
+                port = ChargingPortFeature(
+                    plug=self.PLUG_FOR_CONNECTOR_TYPE[charger["connectorType"]],
+                    power=PowerFeature(
+                        voltage=int(charger["maxVoltage"]),
+                        amperage=int(charger["maxCurrent"]),
+                        output=int(charger["maxPower"]),
+                    ),
+                )
+                evse = EvseFeature(
+                    plugs=[port],
+                    network_id=charger["portId"],
+                )
+
+                ports_for_charging_points[charger["serialNumber"]].append(evse)
+
+        for charging_serial, evses in ports_for_charging_points.items():
+            charging_point = charging_point_serials[charging_serial]
+            charging_point["evses"] = evses
+
+        for charging_point in charging_point_serials.values():
+            charging_points.append(charging_point)
+
+        yield StationFeature(
+            name=station["locationName"],
+            network="BLINK",
+            network_id=station["locationId"],
+            address=address,
+            location=coordinates,
+            charging_points=charging_points,
+        )
+
+    def hardware_for_serial_number(self, serial_number, charger):
+        brand = "BLINK"
+        manufacturer = None
+        model = None
+
+        if serial_number.startswith("BAE"):
+            brand = "SEMA_CONNECT"
+            manufacturer = "SEMA_CONNECT"
+
+            model_number = serial_number[3:7]
+
+            if model_number[0] == "0":
+                model_number = model_number[1:]
+            else:
+                model_number = model_number[:3]
+
+            if model_number.startswith("6") or model_number.startswith("9"):
+                model = "Series 6"
+
+                if model_number in [
+                    "601", "901",
+                    "602", "902",
+                    "603", "903",
+                ]:
+                    brand = "CHARGEPRO"
+            elif model_number.startswith("7"):
+                model = "Series 7"
+            elif model_number.startswith("8"):
+                model = "Series 8"
+            else:
+                raise Exception(charger)
+        elif serial_number.startswith("L1-") or serial_number.startswith("L2-"):
+            manufacturer = "BLINK"
+
+            model_number = int(serial_number.split("-")[1])
+
+            model_map = {
+                202: "MQ 200",
+                203: "MQ 200",
+                205: "HQ 200",
+                207: "IQ 200",
+            }
+
+            if model_number in model_map:
+                model = model_map[model_number]
+            else:
+                raise Exception(charger)
+        elif serial_number.startswith("CTX"):
+            manufacturer = "TELLUS_POWER_GREEN"
+            model = f"TP5-{int(charger["maxPower"] / 1000)}-480"
+        elif serial_number.startswith("NAMT53"):
+            manufacturer = "ABB"
+            model = "Terra 53"
+        else:
+            raise Exception(charger)
+
+        return HardwareFeature(
+            brand=brand,
+            manufacturer=manufacturer,
+            model=model,
+        )

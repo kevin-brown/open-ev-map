c5cec10cda21fb32b8bfe318aec6a770cd559946
Kevin Brown-Silva <github@kevin-brown.com>
2024-11-04 13:48:10 -0500

    Shell Recharge is not OCPI compliant

--- scrapers/spiders/shellrecharge.py
+++ scrapers/spiders/shellrecharge.py
@@ -1,17 +1,14 @@
-from scrapers.items import AddressFeature, ChargingPointFeature, ChargingPortFeature, EvseFeature, HardwareFeature, LocationFeature, PowerFeature, StationFeature
-from scrapers.spiders.ocpi import OcpiSpider
+from scrapers.items import AddressFeature, ChargingPointFeature, ChargingPortFeature, EvseFeature, HardwareFeature, LocationFeature, PowerFeature, SourceFeature, StationFeature
 
 import scrapy
 
 
-class ShellRechargeSpider(OcpiSpider):
+class ShellRechargeSpider(scrapy.Spider):
     name = "shellrecharge"
-    network="SHELL_RECHARGE"
-    system = "SHELL_RECHARGE_GREENLOTS"
 
-    PLUG_TYPE_TO_STANDARD_MAP = {
-        "SAEJ1772": "IEC_62196_T1",
-        "CCS": "IEC_62196_T1_COMBO",
+    PLUG_TYPE_TO_PLUG_MAP = {
+        "SAEJ1772": "J1772_CABLE",
+        "CCS": "J1772_COMBO",
         "CHAdeMO": "CHADEMO",
     }
 
@@ -37,27 +34,63 @@ class ShellRechargeSpider(OcpiSpider):
         stations = response.json()["data"]
 
         for station in stations:
-            station["coordinates"] = {
-                "latitude": station["latitude"],
-                "longitude": station["longitude"],
-            }
+            location = LocationFeature(
+                latitude=station["latitude"],
+                longitude=station["longitude"],
+            )
+            address = AddressFeature(
+                street_address=station["address"],
+                city=station["city"],
+                state=station["state"],
+                zip_code=station["zipCode"],
+            )
 
-            station["postal_code"] = station["zipCode"]
-            station["id"] = station["locationId"]
+            charging_points = []
 
-            for evse in station["evses"]:
-                evse["connectors"] = evse["ports"]
-                evse["physical_reference"] = evse["evseDisplayId"]
-                evse["evse_id"] = evse["evseEmaid"]
+            for station_evse in station["evses"]:
+                plugs = []
 
-                evse["manufacturer"] = evse["evseManufacturerName"]
-                evse["model"] = evse["evseModelName"]
+                for connector in station_evse["ports"]:
+                    power = PowerFeature(
+                        amperage=connector["current"],
+                        voltage=connector["voltage"],
+                        output=connector["maxElectricPower"],
+                    )
 
-                for connector in evse["connectors"]:
-                    connector["standard"] = self.PLUG_TYPE_TO_STANDARD_MAP[connector["plugType"]]
+                    plug = ChargingPortFeature(
+                        plug=self.PLUG_TYPE_TO_PLUG_MAP[connector["plugType"]],
 
-                    connector["max_amperage"] = connector["current"]
-                    connector["max_voltage"] = connector["voltage"]
-                    connector["max_electric_power"] = connector["maxElectricPower"]
+                        power=power,
+                    )
+                    plugs.append(plug)
 
-            yield from self.station_to_feature(station)
+                evse = EvseFeature(
+                    plugs=plugs,
+                )
+
+                hardware = HardwareFeature(
+                    manufacturer=station_evse["evseManufacturerName"],
+                    model=station_evse["evseModelName"],
+                )
+
+                charging_point = ChargingPointFeature(
+                    name=station_evse["evseDisplayId"],
+                    network_id=station_evse["evseEmaid"],
+                    location=location,
+                    evses=[evse],
+                    hardware=hardware,
+                )
+                charging_points.append(charging_point)
+
+            yield StationFeature(
+                name=station["name"],
+                network="SHELL_RECHARGE",
+                network_id=station["locationId"],
+                location=location,
+                address=address,
+                charging_points=charging_points,
+                source=SourceFeature(
+                    quality="ORIGINAL",
+                    system="SHELL_RECHARGE_GREENLOTS",
+                ),
+            )

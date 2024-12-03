f0f3f0d754cac9acc3bd3ee0a2fa9622e92a7461
Kevin <github@kevin-brown.com>
2024-12-03 00:18:26 +0000

    Handle AFDC where it has non-existent stations

--- open-ev-map/src/routes/+page.svelte
+++ open-ev-map/src/routes/+page.svelte
@@ -118,23 +118,29 @@
       return "gray";
     }
 
-    let referenceSources = station.properties.references.map((r) => r.name);
+    let referenceSources = new Set(station.properties.references.map((r) => r.name));
 
-    if (referenceSources.includes("OPEN_STREET_MAP")) {
+    if (referenceSources.has("OPEN_STREET_MAP")) {
       return "green";
     }
 
-    if (referenceSources.includes("OPEN_CHARGE_MAP")) {
+    if (referenceSources.has("OPEN_CHARGE_MAP")) {
       return "blue";
     }
 
-    if (referenceSources.includes("ALTERNATIVE_FUELS_DATA_CENTER")) {
+    if (referenceSources.has("ALTERNATIVE_FUELS_DATA_CENTER")) {
       if (station.properties.network_id?.length > 0) {
         return "orange";
       }
 
       if (!station.properties.network || station.properties.network == "NON_NETWORKED") {
         return "orange";
+      }
+    }
+
+    if (referenceSources.has("ALTERNATIVE_FUELS_DATA_CENTER") && referenceSources.size === 1) {
+      if (station.properties.network == "AMP_UP" || station.properties.network == "TESLA_DESTINATION") {
+        return "";
       }
     }
 
@@ -156,12 +162,14 @@
   {:then stationMarkers}
     {#each stationMarkers as stationMarker}
       <Marker lngLat={latLngForStation(stationMarker)}>
+        {#if stationMarkerColor(stationMarker)}
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <path
             fill={stationMarkerColor(stationMarker)}
             d="M14 11.5A2.5 2.5 0 0 0 16.5 9A2.5 2.5 0 0 0 14 6.5A2.5 2.5 0 0 0 11.5 9a2.5 2.5 0 0 0 2.5 2.5M14 2c3.86 0 7 3.13 7 7c0 5.25-7 13-7 13S7 14.25 7 9a7 7 0 0 1 7-7M5 9c0 4.5 5.08 10.66 6 11.81L10 22S3 14.25 3 9c0-3.17 2.11-5.85 5-6.71C6.16 3.94 5 6.33 5 9Z"
           />
         </svg>
+        {/if}
 
         <Popup openOn="click">
             <h3 class="font-bold text-lg">{ firstPropertyValueForDisplay(stationMarker.properties.name) }</h3>

c3c7b8a78fa06b4a7ca56228f24cd4c538d8d4cf
Kevin <github@kevin-brown.com>
2024-12-07 04:33:16 +0000

    Add charging points/port layout to station edit page

--- open-ev-map/src/routes/networks/[network]/stations/[station]/edit/+page.svelte
+++ open-ev-map/src/routes/networks/[network]/stations/[station]/edit/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { base } from "$app/paths";
+
   function stationName(station): string {
     return station.properties.name && station.properties.name.length > 0 ? station.properties.name[0].value : "";
   }
@@ -15,7 +17,7 @@
   };
 
   let sourcedProperties = ['name', 'network_id'];
-  let unsourcedProperties = ['network'];
+  let unsourcedProperties = ['charging_points', 'network'];
 
   sourcedProperties.forEach((propName) => {
     if (data.station.properties[propName] && data.station.properties[propName].length > 0) {
@@ -60,6 +62,77 @@
         stationDiff += propName + '-';
       }
     }
+  }
+
+  function managedChargingPoint(chargingPoint) {
+    if (!chargingPoint.charging_groups || chargingPoint.charging_groups.length === 0) {
+      return false;
+    }
+
+    if (chargingPoint.charging_groups.some((group) => group.network_id != null)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function portLayoutForChargingPoint(chargingPoint) {
+    let ports = [];
+
+    if (!chargingPoint.charging_groups) {
+      return "";
+    }
+
+    chargingPoint.charging_groups.forEach((group) => {
+      if (!group.ports) {
+        return;
+      }
+
+      group.ports.forEach((port) => {
+        if (!port.plug_type) {
+          return;
+        }
+
+        ports.push(port.plug_type);
+      });
+    });
+
+    ports.sort();
+
+    return ports.join(";");
+  }
+
+  function setPortLayoutForChargingPoint(index, portLayout) {
+    let chargingGroups = [];
+    let ports = portLayout.split(";");
+
+    ports.forEach((port) => {
+      chargingGroups.push({
+        ports: [
+          {
+            plug_type: port
+          }
+        ]
+      });
+    });
+
+    let chargingPoint = editStation.charging_points[index];
+
+    chargingPoint.charging_groups = chargingGroups;
+
+    editStation.charging_points[index] = chargingPoint;
+  }
+
+  function iconPathForPlugType(plugType: string): string {
+    const PLUG_TYPE_MAP = {
+      'CHADEMO': 'chademo',
+      'J1772': 'j1772',
+      'J1772_CABLE': 'j1772',
+      'J1772_COMBO': 'j1772-combo',
+      'NACS': 'nacs',
+    }
+
+    return base + '/icons/plug-' + PLUG_TYPE_MAP[plugType] + '.svg';
   }
 </script>
 
@@ -110,11 +183,35 @@
 
   <h3>Charging Points</h3>
   {#each editStation.charging_points as chargingPoint, idx}
-  <label for={'edit-station-charging-points-' + idx + '-name'}>Name</label>
-  <input type="text" bind:value={chargingPoint.name} id={'edit-station-charging-points-' + idx + '-name'} />
-  <button type="button" on:click={() => removeChargingPoint(idx)}>
-    Delete charging point
-  </button>
+  <fieldset disabled={managedChargingPoint(chargingPoint)}>
+    <label for={'edit-station-charging-points-' + idx + '-name'}>Name</label>
+    <input type="text" bind:value={chargingPoint.name} id={'edit-station-charging-points-' + idx + '-name'} />
+
+    <label for={'edit-station-charging-points-' + idx + '-ports'}>Ports</label>
+    <select id={'edit-station-charging-points-' + idx + '-ports'} value={portLayoutForChargingPoint(chargingPoint)} on:change={(e) => setPortLayoutForChargingPoint(idx, e.target.value)}>
+      <option value="">Unknown</option>
+      <option value="J1772">J-1772</option>
+      <option value="J1772_CABLE">J-1772 (with cable)</option>
+      <option value="J1772_SOCKET">J-1772 (socket only)</option>
+      <option value="J1772_CABLE;J1772_CABLE">Dual J-1772 (with cable)</option>
+      <option value="J1772_COMBO">CCS 1</option>
+      <option value="CHADEMO">CHADEMO</option>
+      <option value="CHADEMO;J1772_COMBO">CCS 1 & CHADEMO</option>
+    </select>
+    {#if chargingPoint.charging_groups}
+    {#each chargingPoint.charging_groups as chargingGroup}
+    {#if chargingGroup.ports}
+    {#each chargingGroup.ports as chargingPort}
+    <img src={iconPathForPlugType(chargingPort.plug_type)} alt={chargingPort.plug_type} height="50px" width="50px" class="inline" />
+    {/each}
+    {/if}
+    {/each}
+    {/if}
+
+    <button type="button" on:click={() => removeChargingPoint(idx)}>
+      Delete charging point
+    </button>
+  </fieldset>
   {/each}
   <button type="button" on:click={addEmptyChargingPoint}>
     Add charging point

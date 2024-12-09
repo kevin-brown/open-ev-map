27290b36aeda534f1f75fbcf65ce05f8f64d1e5a
Kevin <github@kevin-brown.com>
2024-12-09 00:21:13 +0000

    Move station name to layout

---
+++ open-ev-map/src/routes/networks/[network]/stations/[station]/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  function stationName(station): string {
+    return station.properties.name && station.properties.name.length > 0 ? station.properties.name[0].value : "";
+  }
+
+  function openStreetMapLink(station): string {
+    let osmReference = station.properties.references.find((ref) => ref.name == "OPEN_STREET_MAP");
+
+    console.log(osmReference, station.properties);
+
+    return osmReference.url;
+  }
+
+  export let data;
+</script>
+
+<h2>{stationName(data.station)}</h2>
+{data.station.properties.network} <a href={openStreetMapLink(data.station)}>Open Street Map</a>
+
+<slot></slot>
--- open-ev-map/src/routes/networks/[network]/stations/[station]/+page.svelte
+++ open-ev-map/src/routes/networks/[network]/stations/[station]/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-
-  function stationName(station): string {
-    return station.properties.name && station.properties.name.length > 0 ? station.properties.name[0].value : "";
-  }
-
-  export let data;
+import { page } from '$app/stores';
 </script>
 
-<h2>{stationName(data.station)}</h2>
-{data.station.properties.network}
 <a href="/networks/{$page.params.network}/stations/{$page.params.station}/edit">Edit</a>
